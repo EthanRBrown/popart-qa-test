80d50d0bb27898ffaa472966791b5c8ab5e8fd7f
Ethan Brown <ethan.brown@popart.com>
2017-06-22 18:40:03 +0000

    Added additional snapshot test for ResistorInputs (handling missing values).

--- src/__tests__/ResistorInputs.test.js
+++ src/__tests__/ResistorInputs.test.js
@@ -6,3 +6,9 @@ it('renders resistance values correctly', () => {
   const comp = renderer.create(<ResistorInputs r1={100} r2={100} requiv={50} />)
   expect(comp).toMatchSnapshot()
 })
+
+it('when values are missing, rendering is appropriate', () => {
+  const comp = renderer.create(<ResistorInputs />)
+  expect(comp).toMatchSnapshot()
+})
+
